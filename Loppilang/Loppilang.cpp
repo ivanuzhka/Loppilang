91e4725a10b0dbecd2fe2a2719e51d89e897ee68
Иван Ольхов <olkhov.ivan@gmail.com>
2023-12-19 22:29:17 +0300

    2.0

--- Loppilang/Loppilang.cpp
+++ Loppilang/Loppilang.cpp
@@ -1,26 +1,225 @@
 ﻿#pragma once
 
-#include "AVLTree.h"
 #include <iostream>
+
+#include "AVLTree.h"
+#include "TIDTree.h"
+#include "TypeOpStack.h"
+
 using std::cout;
+using std::cin;
+
+void print_type(const NodeType& type)
+{
+	if (type == NodeType::ROOT)
+		std::cout << "ROOT";
+	else if (type == NodeType::SCOPE)
+		std::cout << "SCOPE";
+	else if (type == NodeType::FUNC)
+		std::cout << "FUNC";
+	else if (type == NodeType::TYPE)
+		std::cout << "TYPE";
+	else
+		std::cout << "BODY";
+}
+
+void print_current_tid(TIDTree& tid_tree) {
+	Node* current_node = tid_tree.get_current_node();
+
+	cout << "\n\n";
+	print_type(current_node->_type);
+	cout << " Name: " << current_node->_name << "\n";
+
+	cout << "\n\nVars: " << current_node->_tid.get_vars().get_vars().size() << "\n";
+	for (auto& scope : current_node->_tid.get_vars().get_vars())
+	{
+		cout << "name: " << scope.first << " type: " << scope.second << "\n";
+	}
+
+	cout << "\n\nFuncs: " << current_node->_tid.get_funcs().get_funcs().size() << "\n";
+	for (auto& func : current_node->_tid.get_funcs().get_funcs())
+	{
+		cout << "name: " << func.first << " return_type: " << func.second.first << "\n";
+		for (auto& [par, type] : func.second.second)
+		{
+			cout << "\tname: " << par << " type: " << type << "\n";
+		}
+	}
+
+	cout << "\n\nTypes: " << current_node->_tid.get_types().get_types().size() << "\n";
+	for (auto& type : current_node->_tid.get_types().get_types())
+	{
+		cout << "name: " << type.first << "\n";
+		for (auto& [var, var_type] : type.second)
+		{
+			cout << "\tname: " << var << " type: " << var_type << "\n";
+		}
+	}
+
+	cout << "\n\nChildren: " << current_node->_children.size() << "\n";
+	for (auto& child : current_node->_children)
+	{
+		cout << "\t";
+		print_type(child.second->_type);
+		cout << " name: " << child.first << "\n";
+	}
+}
+
+void process() {
+	TIDTree tid_tree;
+
+	print_current_tid(tid_tree);
+	while (true) {
+		std::string command;
+		std::cout << ">>> ";
+		std::cin >> command;
+		if (command == "create_body")
+		{
+			tid_tree.create_tid(NodeType::BODY);
+		}
+		else if (command == "create_scope")
+		{
+			std::string name;
+			std::cin >> name;
+			tid_tree.create_tid(NodeType::SCOPE, name);
+		}
+		else if (command == "create_func")
+		{
+			std::string name;
+			std::cin >> name;
+			tid_tree.create_tid(NodeType::FUNC, name);
+		}
+		else if (command == "create_type")
+		{
+			std::string name;
+			std::cin >> name;
+			tid_tree.create_tid(NodeType::TYPE, name);
+		}
+		else if (command == "leave_tid")
+		{
+			tid_tree.leave_tid();
+		}
+		else if (command == "push_var")
+		{
+			std::string name, type;
+			std::cin >> name >> type;
+			tid_tree.push_var(name, type);
+		}
+		else if (command == "push_func")
+		{
+			std::string name, return_type;
+			std::cin >> name >> return_type;
+			int param_size;
+			std::cin >> param_size;
+			func_parameters params(param_size);
+			for (auto& el : params)
+			{
+				std::cin >> el.first >> el.second;
+			}
+
+			tid_tree.push_func(name, return_type, params);
+		}
+		else if (command == "push_type")
+		{
+			std::string name;
+			std::cin >> name;
+			int fields_size;
+			std::cin >> fields_size;
+			std::map<std::string, std::string> fields;
+			for (int i = 0; i < fields_size; ++i)
+			{
+				std::string f, s;
+				std::cin >> f >> s;
+				fields[f] = s;
+			}
+			tid_tree.push_type(name, fields);
+		}
+		else if (command == "delete_var")
+		{
+			std::string name;
+			std::cin >> name;
+			tid_tree.delete_var(name);
+		}
+		else if (command == "push_jump")
+		{
+			std::string s;
+			cin >> s;
+			tid_tree.push_jump(s);
+		}
+		else if (command == "check_jumps")
+		{
+			tid_tree.check_jumps();
+		}
+		else
+		{
+			continue;
+		}
+		print_current_tid(tid_tree);
+	}
+}
+
+void print_stack(const TypeOpStack& st)
+{
+	cout << "\n" << st.size() << "\n";
+	for (auto& el : st.get_stack())
+	{
+		cout << el << " ";
+	}
+	cout << "\n\n";
+}
+
+void process_stack() {
+	TypeOpStack st;
+
+	while (true) {
+		cout << ">>> ";
+		std::string s;
+		cin >> s;
+		if (s == "push")
+		{
+			std::string lex;
+			cin >> lex;
+			st.push(lex);
+		}
+		else if (s == "check_bin")
+		{
+			cout << st.check_bin() << "\n";
+		}
+		else if (s == "check_uno")
+		{
+			cout << st.check_uno() << "\n";
+		}
+		else if (s == "checl_assignment")
+		{
+			cout << st.check_assignment() << "\n";
+		}
+		else if (s == "eq_bool")
+		{
+			cout << st.eq_bool() << "\n";
+		}
+		else if (s == "clear")
+		{
+			st.clear();
+		}
+		else {
+			continue;
+		}
+		print_stack(st);
+	}
+}
 
 int main()
 {
-	AVLTree tree;
-
-	tree.insert("a", 1);
-	tree.insert("aa", 2);
-	tree.insert("ab", 1);
-	tree.insert("aaaaa", 3);
-
-	cout << tree.count("a") << "\n";
-	cout << tree.count("aba") << "\n";
-	cout << tree.count("ab") << "\n";
-	cout << "\n";
-	
-	tree.bfs_out();
-
-	cout << "\n";
-	cout << tree.get_type("aaaaa") << "\n";
-	cout << tree.get_type("abcd") << "\n";
+	try
+	{
+		process();
+	}
+	catch (std::invalid_argument error)
+	{
+		std::cerr << "Invalid argument: " << error.what() << "\n";
+	}
+	catch (std::logic_error error)
+	{
+		std::cerr << "Logic error: " << error.what() << "\n";
+	}
 }
---
+++ Loppilang/TypeOpStack.cpp
@@ -0,0 +1,205 @@
+#include "TypeOpStack.h"
+
+std::string TypeOpStack::const_parser(const std::string& s) const
+{
+    if (s.size() <= 6) return s;
+
+    std::string pref = "";
+    for (int i = 0; i < 6; ++i)
+        pref += s[i];
+
+    if (pref != "const_") return s;
+
+    std::string res = "";
+    for (int i = 6; i < (int)s.size(); ++i)
+        res += s[i];
+    return res;
+}
+
+std::string TypeOpStack::max_byte(const std::string& l, const std::string r) const
+{
+    if (l.size() == 4 && r.size() == 4)
+        return "byte";
+    else if (l.size() == 4)
+        return r;
+    else if (r.size() == 4)
+        return l;
+    else
+        return l.back() > r.back() ? l : r;
+}
+
+std::string TypeOpStack::check_bin(std::string s1, std::string s2, std::string op) const
+{
+    if (_assign_ops.count(op))
+    {
+        return check_assignment(s1, s2, op);
+    }
+
+    s1 = const_parser(s1);
+    s2 = const_parser(s2);
+
+    if (!_built_in_types.count(s1) || !_built_in_types.count(s2))
+        throw std::logic_error("incorrect types: " + s1 + " " + s2);
+
+    if (s1 != s2 && (s1 == "string" || s2 == "string" || s1 == "array" || s2 == "array"))
+        throw std::logic_error(s1 + " " + op + " " + s2);
+
+    if (s1 == s2 && s1 == "string")
+    {
+        if (op == "+")
+            return "string";
+        else if (_comp_ops.count(op))
+            return "byte";
+    }
+    else if (s1 == s2 && s1 == "array")
+    {
+        if (op == "==" || op == "!=")
+            return "byte";
+    }
+    else if (s1 != s2)
+    {
+        if (_comp_ops.count(op) || _logic_bin_ops.count(op))
+            return "byte";
+        else if (_bin_ops.count(op))
+            return max_byte(s1, s2);
+    }
+    else
+    {
+        throw std::logic_error(s1 + " " + op + " " + s2);
+    }
+}
+
+std::string TypeOpStack::check_uno(std::string s, std::string op) const
+{
+    if (!_const_built_in_types.count(s) && !_built_in_types.count(s))
+        throw std::invalid_argument("incorrect type: " + s);
+
+    if (!_uno_ops.count(op) && !_logic_uno_ops.count(op))
+        throw std::invalid_argument("incorrect operation: " + op);
+
+    if (s == "string" || s == "array")
+        throw std::logic_error(s + " " + op);
+
+    if (s != const_parser(s) && (op == "++" || op == "--"))
+        throw std::logic_error(s + " " + op);
+
+    if (_logic_uno_ops.count(op))
+        return "byte";
+
+    return s;
+}
+
+std::string TypeOpStack::check_assignment(std::string s1, std::string s2, std::string op) const
+{
+    if (_const_built_in_types.count(s1))
+        throw std::logic_error("you can't change const types");
+
+    s1 = const_parser(s1);
+    s2 = const_parser(s2);
+
+    if (!_built_in_types.count(s1) || !_built_in_types.count(s2))
+        throw std::logic_error("incorrect types: " + s1 + " " + s2);
+
+    if (s1 != s2 && (s1 == "string" || s2 == "string" || s1 == "array" || s2 == "array"))
+        throw std::logic_error(s1 + " " + op + " " + s2);
+
+    if (s1 == s2 && s1 == "string")
+    {
+        if (op == "+=" && op == "=")
+            return "string";
+    }
+    else if (s1 != s2)
+    {
+        if (_assign_ops.count(op) && max_byte(s1, s2) == s1)
+            return s1;
+    }
+    else
+    {
+        throw std::logic_error(s1 + " " + op + " " + s2);
+    }
+}
+
+bool TypeOpStack::is_op(std::string s) const
+{
+    if (_built_in_types.count(s) || _const_built_in_types.count(s))
+        return false;
+
+    if (_comp_ops.count(s) ||
+        _logic_bin_ops.count(s) ||
+        _logic_uno_ops.count(s) ||
+        _bin_ops.count(s) ||
+        _uno_ops.count(s) ||
+        _assign_ops.count(s))
+        return true;
+
+    throw std::invalid_argument("incorrect lexem: " + s);
+}
+
+
+void TypeOpStack::push(std::string lexem)
+{
+    is_op(lexem);
+    _stack.push_back(lexem);
+}
+
+std::string TypeOpStack::check_bin()
+{
+    if (_stack.size() < 3)
+        throw std::logic_error("too few elements");
+
+    std::string s1, s2, op;
+    s2 = _stack.back(); _stack.pop_back();
+    op = _stack.back(); _stack.pop_back();
+    s1 = _stack.back(); _stack.pop_back();
+
+    return check_bin(s1, s2, op);
+}
+
+std::string TypeOpStack::check_uno()
+{
+    if (_stack.size() < 2)
+        throw std::logic_error("too few elements");
+
+    std::string s, op;
+    s = _stack.back(); _stack.pop_back();
+    op = _stack.back(); _stack.pop_back();
+
+    return check_uno(s, op);
+}
+
+std::string TypeOpStack::check_assignment()
+{
+    if (_stack.size() < 3)
+        throw std::logic_error("too few elements");
+
+    std::string s1, s2, op;
+    s2 = _stack.back(); _stack.pop_back();
+    op = _stack.back(); _stack.pop_back();
+    s1 = _stack.back(); _stack.pop_back();
+
+    return check_assignment(s1, s2, op);
+}
+
+std::string TypeOpStack::eq_bool() const
+{
+    if (_stack.empty())
+        throw std::logic_error("too few elements");
+
+    std::string s = _stack.back();
+    if (is_op(s))
+        throw std::logic_error("top of the stack: " + s);
+
+    std::string new_s = const_parser(s);
+    while (new_s.size() > 4)
+        new_s.pop_back();
+    if (new_s == "byte")
+        return s;
+
+    throw std::logic_error(s + " isn't bool(byte)");
+}
+
+void TypeOpStack::clear()
+{
+    while (!_stack.empty())
+        _stack.pop_back();
+}
